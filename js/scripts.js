db3317c0a380257b26ca432b7f33777b61f360c0
Julien Law <julien@pagefreezer.com>
2019-11-19 15:52:43 -0800

    remove scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var bane = document.getElementById('bane');
-var cia = document.getElementById('cia');
+//var bane = document.getElementById('bane');
+//var cia = document.getElementById('cia');
 
-bane.src = bane.getAttribute('data-pf-src');
-cia.src = cia.getAttribute('data-pf-src');
+//bane.src = bane.getAttribute('data-pf-src');
+//cia.src = cia.getAttribute('data-pf-src');
